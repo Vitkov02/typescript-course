f17e6acbdb8290be491543feec680fb0d31ef5b0
aesthxtics <vitkowova@gmail.com>
2022-07-17 19:05:14 +0300

    :)

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { FavouritePage } from './pages/FavouritePage';
+import { HomePage } from './pages/HomePage';
+import {Navigation} from './components/Navigation';
+
+function App() {
+  return (
+  <>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/favourites' element={<FavouritePage />} />
+    </Routes>
+  </>
+  )
+}
+
+export default App;
